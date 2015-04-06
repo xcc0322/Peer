ed83b3e023a6471ab155ca8d1f59a62513a19cb1
xcc0322 <xcc0322@gmail.com>
2015-04-07 00:19:35 +0800

    Implement TodoList create, edit, delete flow.

--- app/src/main/java/com/xcc0322/peer/HomeActivity.java
+++ app/src/main/java/com/xcc0322/peer/HomeActivity.java
@@ -6,8 +6,8 @@ import android.app.Dialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -22,179 +22,130 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class HomeActivity extends ListActivity {
-    private static final int ACTIVITY_CREATE = 0;
-    private static final int ACTIVITY_EDIT = 1;
+  private static final String TAG = "HomeActivity";
+  private static final int ACTIVITY_CREATE = 0;
+  private static final int ACTIVITY_EDIT = 1;
 
-    public static final int INSERT_ID = Menu.FIRST;
-    private static final int DELETE_ID = Menu.FIRST + 1;
+  public static final int INSERT_ID = Menu.FIRST;
+  private static final int DELETE_ID = Menu.FIRST + 1;
 
-    private List<ParseObject> todos;
-    private Dialog progressDialog;
+  private Dialog progressDialog;
+  List<ParseObject> todos;
 
-    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
-        // Override this method to do custom remote calls
-        protected Void doInBackground(Void... params) {
-            // Gets the current list of todos in sorted order
-            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Todo");
-            query.orderByDescending("_created_at");
+  private Void delete(ParseObject todo) {
+    try {
+      todo.delete();
+    } catch (ParseException e) {
+      Log.e(TAG, e.getMessage());
+    }
+    return null;
+  }
 
-            try {
-                todos = query.find();
-            } catch (ParseException e) {
+  private Void getTodoList() {
+    ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Todo");
+    query.orderByDescending("_created_at");
+    try {
+        todos = query.find();
+    } catch (ParseException e) {
+        Log.e(TAG, e.getMessage());
+    }
+    return null;
+  }
 
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            HomeActivity.this.progressDialog = ProgressDialog.show(HomeActivity.this, "",
-                    "Loading...", true);
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            // Put the list of todos into the list view
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomeActivity.this,
-                    R.layout.todo_row);
-            if (todos != null) {
-                for (ParseObject todo : todos) {
-                    adapter.add((String) todo.get("name"));
-                }
-            }
-            setListAdapter(adapter);
-            HomeActivity.this.progressDialog.dismiss();
-            TextView empty = (TextView) findViewById(android.R.id.empty);
-            empty.setVisibility(View.VISIBLE);
+  protected void updateUI() {
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomeActivity.this,
+            R.layout.todo_row);
+    if (todos != null) {
+        for (ParseObject todo : todos) {
+            adapter.add((String) todo.get("name"));
         }
     }
+    setListAdapter(adapter);
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+    progressDialog.dismiss();
+    TextView empty = (TextView) findViewById(android.R.id.empty);
+    empty.setVisibility(View.VISIBLE);
+  }
 
-        TextView empty = (TextView) findViewById(android.R.id.empty);
-        empty.setVisibility(View.INVISIBLE);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.home);
+    registerForContextMenu(getListView());
+  }
 
-        new RemoteDataTask().execute();
-        registerForContextMenu(getListView());
+  @Override
+  public void onResume() {
+    super.onResume();
+    updateToDoList();
+  }
+
+  private void updateToDoList() {
+    TextView empty = (TextView) findViewById(android.R.id.empty);
+    empty.setVisibility(View.INVISIBLE);
+    progressDialog = ProgressDialog.show(
+        HomeActivity.this, "", getString(R.string.loading), true);
+
+    Observable.just(getTodoList())
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+            r -> {
+              if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+              }
+              updateUI();
+            });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    boolean result = super.onCreateOptionsMenu(menu);
+    menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+    return result;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case INSERT_ID:
+        startActivity(new Intent(this, CreateToDo.class));
+        return true;
     }
+    return super.onOptionsItemSelected(item);
+  }
 
-    private void createTodo() {
-        Intent i = new Intent(this, CreateToDo.class);
-        startActivityForResult(i, ACTIVITY_CREATE);
+  @Override
+  public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+    super.onCreateContextMenu(menu, v, menuInfo);
+    menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+  }
+
+  @Override
+  public boolean onContextItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+        case DELETE_ID:
+            AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+            final ParseObject todo = todos.get(info.position);
+            Observable.just(delete(todo))
+              .subscribeOn(Schedulers.io())
+              .observeOn(AndroidSchedulers.mainThread())
+              .subscribe(r -> {updateToDoList();});
+            return true;
     }
+    return super.onContextItemSelected(item);
+  }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        if (intent == null) {
-            return;
-        }
-        final Bundle extras = intent.getExtras();
-
-        switch (requestCode) {
-            case ACTIVITY_CREATE:
-                new RemoteDataTask() {
-                    protected Void doInBackground(Void... params) {
-                        String name = extras.getString("name");
-                        ParseObject todo = new ParseObject("Todo");
-                        todo.put("name", name);
-                        try {
-                            todo.save();
-                        } catch (ParseException e) {
-                        }
-
-                        super.doInBackground();
-                        return null;
-                    }
-                }.execute();
-                break;
-            case ACTIVITY_EDIT:
-                // Edit the remote object
-                final ParseObject todo;
-                todo = todos.get(extras.getInt("position"));
-                todo.put("name", extras.getString("name"));
-
-                new RemoteDataTask() {
-                    protected Void doInBackground(Void... params) {
-                        try {
-                            todo.save();
-                        } catch (ParseException e) {
-                        }
-                        super.doInBackground();
-                        return null;
-                    }
-                }.execute();
-                break;
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        boolean result = super.onCreateOptionsMenu(menu);
-        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
-        return result;
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case DELETE_ID:
-                AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-
-                // Delete the remote object
-                final ParseObject todo = todos.get(info.position);
-
-                new RemoteDataTask() {
-                    protected Void doInBackground(Void... params) {
-                        try {
-                            todo.delete();
-                        } catch (ParseException e) {
-                        }
-                        super.doInBackground();
-                        return null;
-                    }
-                }.execute();
-                return true;
-        }
-        return super.onContextItemSelected(item);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case INSERT_ID:
-                createTodo();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent i = new Intent(this, CreateToDo.class);
-
-        i.putExtra("name", todos.get(position).getString("name").toString());
-        i.putExtra("position", position);
-        startActivityForResult(i, ACTIVITY_EDIT);
-    }
-
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(l, v, position, id);
+    Intent i = new Intent(this, CreateToDo.class);
+    i.putExtra("todoId", todos.get(position).getObjectId());
+    startActivity(i);
+  }
 }
---
+++ app/src/main/java/com/xcc0322/peer/PeerApplication.java
@@ -0,0 +1,36 @@
+package com.xcc0322.peer;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class PeerApplication extends Application {
+    private static final String APPLICATION_ID =
+            "hFyJ3QxrARb3j64sXwXwp49s5QFolaYJCmtPZaGU";
+    private static final String CLIENT_KEY =
+            "K4qm3Xnahm6WvMNToVJRJLr346rPVHnmTK4ktyKB";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize Crash Reporting.
+        ParseCrashReporting.enable(this);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        defaultACL.setPublicReadAccess(true);
+        // ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
